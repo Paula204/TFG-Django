c8d614c72616ae10e81318c1497b5b9c5755697a
Paula204 <pbm20496@hotmail.com>
2019-07-08 17:55:39 +0200

    as

--- web/apps/tfg/models.py
+++ web/apps/tfg/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class News(models.Model):
-    id = models.AutoField(primary_key= True)
-    fechaEntrada = models.DateField('Fecha de creación', auto_now= True, auto_now_add= False)
-    url = models.URLField(blank= False, null= False)
-    esFi =  models.BooleanField(null= False, default= False)
+    id = models.AutoField(primary_key=True)
+    fechaEntrada = models.DateField('Fecha de creación', auto_now=True, auto_now_add=False)
+    url = models.URLField(blank=False, null=False)
+    esFi = models.BooleanField(null=False, default=False)
 
     class Meta:
         verbose_name = 'News'
